a4def3f872c0c2448f09cbbeb37cb38d3fb61084
Salman-Talib7 <21309782@stu.mmu.ac.uk>
2024-12-06 15:11:07 +0000

    Replaced Router with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import './css/App.css';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
